6576d5cb3433394da472387deae97c4025459a7b
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2020-10-01 16:42:49 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <ncurses.h>
+#include <pthread.h>
+#define endl '\n'
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
