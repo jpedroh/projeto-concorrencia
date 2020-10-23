4d14120fa51c94c40534ec0aa628e5029986f812
Joao Pedro <joao.pedro.hsd@gmail.com>
2020-10-23 09:56:12 -0300

    Migramos para ncurses aaaaa

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@
 
 using namespace std;
 
+WINDOW *win1;
+WINDOW *win2;
+WINDOW *win3;
+
+int maxX, maxY;
+
 struct Musica
 {
     string nome;
@@ -15,28 +21,6 @@ struct Musica
 
 vector<Musica> fila;
 pthread_mutex_t mutexx = PTHREAD_MUTEX_INITIALIZER;
-
-void* ui(void* arg) {
-    while(true) {                                                       //o laco eh para simular a tela de reproducao do player, sempre
-        while(pthread_mutex_trylock(&mutexx));                          //exibindo a fila de reproducao
-                                                                        
-        cout << "# \t Artista \t Musica \t Duracao" << endl;
-        if(fila.empty()) {
-            cout << "A fila de execucao esta vazia :(" << endl;
-        } else {
-            int i = 0;
-            for(auto musica : fila) {
-                int minutos = musica.duracao_em_segundos / 60;          //para uma exibicao mais facil dividimos a duracao em minutos e segundos
-                int segundos = musica.duracao_em_segundos % 60;
-
-                cout << i << '\t' << musica.artista << '\t' << musica.nome << '\t' << minutos << ':' << segundos <<  endl;
-                i++;
-            }
-        }
-        pthread_mutex_unlock(&mutexx);
-        sleep(1);                                                       //o sleep eh para que mensagem nao seja imprimida mais de uma vez antes de 
-    }                                                                   //receber o primeiro input do usuario
-}
 
 void adicionar_musica() {
     int minutos;
@@ -64,33 +48,96 @@ void remover_musica() {
     fila.erase(fila.begin() + indice);
 }
 
-void* receberInput(void* arg) {
-    while(true) {                                                           //o laco eh para simular a tela de reproducao do player, sempre dando a
-        while(pthread_mutex_trylock(&mutexx));                              //opcao de escolher uma acao
-                                                                            //o mutex/trylock eh para que somente uma musica possa ser 
-            cout << "Escolha uma opção" << endl;                            //adicionada ou removida por vez
-            cout << "A de Add \t R de Remove \t Q de Quit" << endl;
-            char selecao;
-            cin >> selecao;
+void imprimir_fila_de_reproducao() {
+    wclear(win1);
+    box(win1, '*', '*');
 
-            switch (selecao) {
-                case 'a':
-                case 'A':
-                    adicionar_musica();
-                    break;
-                case 'R':
-                case 'r':
-                    remover_musica();
-                    break;
-                case 'q':
-                case 'Q':
-                    exit(0);
-                    break;
-                default:
-                    cout << "Opcao invalida :(" << endl;
-            }
+    int safeX = int(maxX - 8)/6;
 
-        pthread_mutex_unlock(&mutexx);
+    mvwprintw(win1, 1, 2, "#");
+    mvwprintw(win1, 1, 6, "Musica");
+    mvwprintw(win1, 1, 3*safeX, "Artista");
+    mvwprintw(win1, 1, 5*safeX, "Duracao");
+
+    if(fila.empty()) {
+        mvwprintw(win1, (maxY - 6) / 2, (maxX/2) - 11, "Nenhuma música na fila");
+    }
+
+    for(int i = 0; i < fila.size(); i++) {
+        Musica musica = fila.at(i);
+
+        mvwprintw(win1, i + 2, 2, "%d", i);
+        mvwprintw(win1, i + 2, 6, musica.nome.data());
+        mvwprintw(win1, i + 2, 3*safeX, musica.artista.data());
+
+        int minutos = musica.duracao_em_segundos / 60;
+        int segundos = musica.duracao_em_segundos % 60;
+        mvwprintw(win1, i + 2, 5*safeX, "%d:%02d", minutos, segundos);
+    }
+
+    wrefresh(win1);
+}
+
+void receber_input() {
+    int safeX = int(maxX - 4)/5;
+    
+    wmove(win3, 1, 0);
+    wclrtoeol(win3);
+    mvwprintw(win3, 1, 2, "A for ADD");
+    mvwprintw(win3, 1, safeX, "R for REMOVE");
+    mvwprintw(win3, 1, 2*safeX, "P for PLAY/PAUSE");
+    mvwprintw(win3, 1, 3*safeX, "S for SKIP");
+    mvwprintw(win3, 1, 4*safeX, "Q for QUIT");
+    wrefresh(win3);
+    char option = wgetch(win3);
+
+    if(option == 'Q' || option == 'q') {
+        endwin();
+        pthread_exit(0);
+    } else if(option == 'A' || option == 'a') {
+        Musica musicaLeitura;
+
+        char nome[20];
+        char artista[20];
+
+        wmove(win3, 1, 0);
+        wclrtoeol(win3);
+        mvwprintw(win3, 1, 2, "Digite o nome da musica: ");
+        wrefresh(win3);
+        wgetnstr(win3, nome, 20);
+        musicaLeitura.nome = string(nome);
+
+        wmove(win3, 1, 0);
+        wclrtoeol(win3);
+        mvwprintw(win3, 1, 2, "Digite o nome do artista: ");
+        wrefresh(win3);
+        wgetnstr(win3, artista, 20);
+        musicaLeitura.artista = string(artista);
+        
+        int minutos, segundos;
+        wmove(win3, 1, 0);
+        wclrtoeol(win3);
+        mvwprintw(win3, 1, 2, "Digite a duração da musica em minutos e depois em segundos: ");
+        wrefresh(win3);
+        wscanw(win3, "%d %d", &minutos, &segundos);
+        musicaLeitura.duracao_em_segundos = minutos * 60 + segundos;
+        fila.emplace_back(musicaLeitura);
+    } else if(option == 'R' || option == 'r') {
+        int indice;
+        
+        wmove(win3, 1, 0);
+        wclrtoeol(win3);
+        mvwprintw(win3, 1, 2, "Digite o índice da musica a ser removida: ");
+        wrefresh(win3);
+        wscanw(win3, "%d", &indice);
+        fila.erase(fila.begin() + indice);
+    }
+}
+
+void* ui(void* arg) {
+    while(true) {
+        imprimir_fila_de_reproducao();
+        receber_input();
     }
 }
 
@@ -99,11 +146,25 @@ int main(int argc, char *argv[]) {
 
     pthread_t threads[2];
 
-    pthread_create(&threads[0], NULL, &ui, NULL);                       //primeira thread responsavel pela interface, exibindo a tela do player
-    pthread_create(&threads[1], NULL, &receberInput, NULL);             //segunda thread responsavel por lidar com os inputs do usuario
+    initscr();
+    getmaxyx(stdscr, maxY, maxX);
 
-    pthread_join(threads[0], NULL);
+    win1 = newwin(maxY - 6, maxX, 0, 0);
+    win2 = newwin(3, maxX, maxY - 6, 0);
+    win3 = newwin(3, maxX, maxY - 3, 0);
+    refresh();
+
+    box(win1, '*', '*');
+    box(win2, '*', '*');
+    box(win3, '*', '*');
+    wrefresh(win1);
+    wrefresh(win2);
+    wrefresh(win3);
+      
+    pthread_create(&threads[1], NULL, &ui, NULL);
     pthread_join(threads[1], NULL);
+
+    endwin();
 
     return 0;
 }
